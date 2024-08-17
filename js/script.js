01ec6f47da9a7c53db54ed40b9c9af6b537d625b
Caleb775 <89751339+Caleb775@users.noreply.github.com>
2024-08-17 18:34:11 +0900

    notes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 /* This is how you leave comments in javascript */
-/*  */
+
+/* Below is how you make a variable and have a string "" attached to it*/
 let read = "Read this Variable";
+/* you can use '' or "" */
+//let read = "Read this Variable"; or let read = 'Read this Variable';
+// to log to the console you will use
+console.log(read);
+//When a variable is declared with the let keyword, you can reassign (or change the value of) that variable later
+// when you do reasign a let variable there is no need to use let again
+read = "Reread this Variable";
